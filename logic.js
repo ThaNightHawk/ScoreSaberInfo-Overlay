500a23e8277c864282b5197d994f9c54a744b2cb
NightHawk <hasfeldt@yahoo.dk>
2022-10-14 21:29:08 +0200

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,14 +1,16 @@
-  fetch('https://new.scoresaber.com/api/player/76561198086326146/basic')
+const userId = "76561198086326146";
+fetch('https://new.scoresaber.com/api/player/'+userId+'/basic')
         .then(response => response.json())
         .then(data => {
             let playerId = data.playerInfo.playerId;
             let playerCountry = data.playerInfo.country;
             let playerGlobalRank = data.playerInfo.rank;
             let playerCountryRank = data.playerInfo.countryRank;
+            let avatar = data.playerInfo.avatar;
             if (data.playerInfo.avatar == "/images/oculus.png") {
                 document.getElementById("PlayerImage").src = "https://new.scoresaber.com/api/static/avatars/oculus.png";
             } else {
-                document.getElementById("PlayerImage").src = "https://new.scoresaber.com/api/static/avatars/" + playerId + ".jpg";
+                document.getElementById("PlayerImage").src = "https://new.scoresaber.com" + avatar;
 				
             }
 			document.getElementById("PlayerFlag").src = "https://flagicons.lipis.dev/flags/4x3/" + playerCountry.toLowerCase() + ".svg";
